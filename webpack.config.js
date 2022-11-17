8f591358aa5c3a6485b0d3405c7822a644af0319
yh <>
2022-11-17 15:11:00 +0900

    laravel＋babel＋webpack＋react

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./resources/ts/app.tsx",
+  output: {
+    path: `${__dirname}/public/js`,
+    filename: "app.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: ["ts-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
